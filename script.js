137567a26c67abd09d6a9d009116dc8a616386a5
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-04-13 11:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-function add(a, b) {
- return a + b;
+class Doc extends document {
+  function get_elem(var id) {
+    return document.getElementById(id);
+  }
+
+  function get_elem(var className) {
+    return document.getElementByClassName(className);
+  }
+
+  function get_elem(var tag) {
+    return document.getElementByTag(tag);
+  }
+
+  function get_elems(var id) {
+    return document.getElementsById(id);
+  }
+
+  function get_elems(var className) {
+    return document.getElementsByClassName(className);
+  }
+
+  function get_elems(var tag) {
+    return document.getElementsByTag(tag);
+  }
 };
-console.log(add(5, 5)); //outputs 10
+
+var doc = new Doc();
+
+// same document.getElementById("idname")
+// same doc.get_elem(id="idname");
